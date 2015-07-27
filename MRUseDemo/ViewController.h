1e95b59ebefaa97768045f5fce81b7191c01be57
zeze <18813155491@163.com>
2015-07-27 14:55:43 +0800

    Initial Commit

---
+++ MRUseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRUseDemo
+//
+//  Created by zezefamily on 15/7/27.
+//  Copyright (c) 2015年 zezefamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MRUseDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MRUseDemo
+//
+//  Created by zezefamily on 15/7/27.
+//  Copyright (c) 2015年 zezefamily. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
